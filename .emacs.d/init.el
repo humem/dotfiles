9c633cf09c0d39cb425c197f58558c6c1de7aab7
Hiroshi Umemoto <umemoto@Hiroshis-iMac.local>
2014-12-07 23:19:52 +0900

    disable to act on other window

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -196,10 +196,10 @@ and source-file directory for your debugger." t nil)
     "SPC" 'set-mark-command
 ;    "x" 'execute-extended-command
     "x" 'helm-M-x
-    "b" 'switch-to-buffer-other-window
-    "f" 'find-file-other-window
-    "d" 'dired-other-window
-    "j" 'dired-jump-other-window
+    "b" 'switch-to-buffer
+    "f" 'find-file
+    "d" 'dired
+    "j" 'dired-jump
     "k" 'kill-buffer
     "q" 'View-quit
     "w" 'evil-window-prev
