a04b416eabd22529b6f075a738477e815f1b05f3
Hiroshi Umemoto <hiroshi.umemoto@gmail.com>
2023-01-17 00:53:56 +0900

    Add early-init.el

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,18 @@
+;;; early-init.el --- early-init file for Emacs -*- lexical-binding: t -*-
+
+;;; Code:
+
+;;; Commentary:
+
+;; Debugging
+(setq debug-on-error t)
+;;(setq warning-minimum-level :error)
+
+;; Suppress cl warning
+(setq byte-compile-warnings '(cl-functions))
+
+;; Disable GUI bars
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+
+;;; early-init.el ends here
