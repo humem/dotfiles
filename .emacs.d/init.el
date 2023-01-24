d22514a11f34166289422fdd749da88e86f097d1
Hiroshi Umemoto <hiroshi.umemoto@gmail.com>
2023-01-25 08:56:26 +0900

    Clean up files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,32 @@
 
 ;;; Code:
 
+;;
+;; Personal settings
+;;
+
+(custom-set-variables '(user-full-name "Hiroshi Umemoto"))
+(defvar my/enable-setup-tracker t)
+
+;; (defvar my/enable-profiler t)
+;; (defvar my/fonts-family "HackGenNerd Console")
+;; (custom-set-variables '(gcmh-verbose t))
+
+;; Enable when running on dynabook
+(when (string-equal (getenv "EMACS_MACHINE_NAME") "dynabook")
+  (defvar my/fonts-height 105)
+  (defvar my/battery t)
+  (defvar my/modus-themes 'modus-vivendi)
+  (defvar my/modus-themes-region '(accented no-extend))
+  (custom-set-faces
+   '(fill-column-indicator ((t (:foreground "dim gray"))))
+   '(doom-modeline-bar-inactive ((t (:box (:line-width (2 . 2)))))))
+  (custom-set-variables
+   '(highlight-indent-guides-auto-character-face-perc 30)
+   '(highlight-indent-guides-auto-top-character-face-perc 50))
+  (defvar my/skip t)
+  )
+
 ;; Faster startup (start)
 (defconst my-saved-file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
