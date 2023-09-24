d75dd6601fd2b3ab6b1d4512ea66b59e0687efef
Hiroshi Umemoto <hiroshi.umemoto@gmail.com>
2023-09-24 22:54:54 +0900

    Fix clipboard on wsl

--- astronvim/user/init.lua
+++ astronvim/user/init.lua
@@ -39,6 +39,69 @@ return {
   },
 
   plugins = {
+    -- {
+    --   "zbirenbaum/copilot.lua",
+    --   cmd = "Copilot",
+    --   event = "User AstroFile",
+    --   opts = { suggestion = { auto_trigger = true, debounce = 150 } },
+    -- },
+    -- {
+    --   "hrsh7th/nvim-cmp",
+    --   dependencies = { "zbirenbaum/copilot.lua" },
+    --   opts = function(_, opts)
+    --     local cmp, copilot = require "cmp", require "copilot.suggestion"
+    --     local snip_status_ok, luasnip = pcall(require, "luasnip")
+    --     if not snip_status_ok then return end
+    --     local function has_words_before()
+    --       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    --       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+    --     end
+    --     if not opts.mapping then opts.mapping = {} end
+    --     opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+    --       if copilot.is_visible() then
+    --         copilot.accept()
+    --       elseif cmp.visible() then
+    --         cmp.select_next_item()
+    --       elseif luasnip.expand_or_jumpable() then
+    --         luasnip.expand_or_jump()
+    --       elseif has_words_before() then
+    --         cmp.complete()
+    --       else
+    --         fallback()
+    --       end
+    --     end, { "i", "s" })
+    --
+    --     opts.mapping["<C-x>"] = cmp.mapping(function()
+    --       if copilot.is_visible() then copilot.next() end
+    --     end)
+    --
+    --     opts.mapping["<C-z>"] = cmp.mapping(function()
+    --       if copilot.is_visible() then copilot.prev() end
+    --     end)
+    --
+    --     opts.mapping["<C-right>"] = cmp.mapping(function()
+    --       if copilot.is_visible() then copilot.accept_word() end
+    --     end)
+    --
+    --     opts.mapping["<C-l>"] = cmp.mapping(function()
+    --       if copilot.is_visible() then copilot.accept_word() end
+    --     end)
+    --
+    --     opts.mapping["<C-down>"] = cmp.mapping(function()
+    --       if copilot.is_visible() then copilot.accept_line() end
+    --     end)
+    --
+    --     opts.mapping["<C-j>"] = cmp.mapping(function()
+    --       if copilot.is_visible() then copilot.accept_line() end
+    --     end)
+    --
+    --     opts.mapping["<C-c>"] = cmp.mapping(function()
+    --       if copilot.is_visible() then copilot.dismiss() end
+    --     end)
+    --
+    --     return opts
+    --   end,
+    -- },
     {
       "jay-babu/mason-nvim-dap.nvim",
       opts = {
@@ -206,5 +269,23 @@ return {
     vim.cmd([[set iskeyword-=_]])
     -- concat japanese strings
     vim.cmd([[set formatoptions+=mM]])
+    -- clipboard on WSL
+    vim.cmd([[
+    if !empty($WSL_DISTRO_NAME)
+      let g:clipboard = {
+        \   'name': 'win32yank-wsl',
+        \   'copy': {
+        \      '+': 'win32yank.exe -i --crlf',
+        \      '*': 'win32yank.exe -i --crlf',
+        \    },
+        \   'paste': {
+        \      '+': 'win32yank.exe -o --lf',
+        \      '*': 'win32yank.exe -o --lf',
+        \   },
+        \   'cache_enabled': 0,
+        \ }
+    endif
+    ]])
   end,
 }
+
